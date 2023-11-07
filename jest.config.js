94e77a40ac2371fcd1766e09873ea680ed93c015
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-11-07 22:38:55 +0630

    add test & coverage , change workflow file names

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,9 @@ export default {
 	extensionsToTreatAsEsm: ['.ts'],
 	moduleNameMapper: {
 		'^(\\.{1,2}/.*)\\.js$': '$1',
-	}
-
+	},
+	collectCoverage: true,
+	collectCoverageFrom: ["src/**/*.ts"], // Adjust this to match your source files.
+	coverageDirectory: "coverage",
+	coverageReporters: ["lcov", "text"]
 };
